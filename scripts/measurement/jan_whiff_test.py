57a547dc431a884af18095cf38425fb7acd04f30
jirhiker <jirhiker@gmail.com>
2014-11-13 11:40:50 -0700

    added whiff test

---
+++ scripts/measurement/jan_whiff_test.py
@@ -0,0 +1,143 @@
+#!Measurement
+'''
+baseline:
+  after: true
+  before: false
+  counts: 30
+  detector: H1
+  mass: 39.59
+default_fits: nominal
+multicollect:
+  counts: 200
+  detector: H1
+  isotope: Ar40
+peakcenter:
+  after: false
+  before: false
+  detector: H1
+  isotope: Ar40
+equilibration:
+  inlet: R
+  outlet: S
+  inlet_delay: 3
+  eqtime: 20
+  use_extraction_eqtime: True
+whiff:
+  eqtime: 10
+  counts: 10
+  abbreviated_count_ratio: 0.25
+  conditionals:
+    - action: run
+      attr: age
+      teststr: age>50
+      start: 9
+    - action: run_remainder
+      teststr: age<0.1
+      attr: age
+      start: 9
+'''
+
+#equilibration
+EQ_TIME= 20.0
+
+#PEAK HOP
+USE_PEAK_HOP= False
+
+ACTIVE_DETECTORS=('H2','H1','AX','L1','L2', 'CDD')
+#FITS=('Ar41:linear','Ar40:linear', 'Ar39:parabolic','Ar38:parabolic','Ar37:parabolic','Ar36:parabolic')
+
+def main():
+    #this is a comment
+    '''
+        this is a multiline
+        comment aka docstring
+    '''
+    #display information with info(msg)
+    info('unknown measurement script')
+
+    #set the spectrometer parameters
+    #provide a value
+    set_source_parameters(YSymmetry=10)
+
+    #or leave blank and values are loaded from a config file (setupfiles/spectrometer/config.cfg)
+    set_source_optics()
+
+    #set the cdd operating voltage
+    set_cdd_operating_voltage(100)
+
+    if mx.peakcenter.before:
+        peak_center(detector=mx.peakcenter.detector,isotope=mx.peakcenter.isotope)
+
+    #open a plot panel for this detectors
+    activate_detectors(*ACTIVE_DETECTORS)
+
+    if mx.baseline.before:
+        baselines(ncounts=mx.baseline.counts,mass=mx.baseline.mass, detector=mx.baseline.detector)
+
+
+    #position mass spectrometer
+    position_magnet(mx.multicollect.isotope, detector=mx.multicollect.detector)
+
+    #gas is staged behind inlet
+
+    meqtime = mx.whiff.eqtime
+    #post equilibration script triggered after eqtime elapsed
+    #equilibrate is non blocking
+    #so use either a sniff of sleep as a placeholder until eq finished
+    equilibrate(eqtime=meqtime, do_post_equilibration=False,
+                inlet=mx.equilibration.inlet, outlet=mx.equilibration.outlet)
+
+    #equilibrate returns immediately after the inlet opens
+    set_time_zero(0)
+
+    sniff(meqtime)
+    #set default regression
+    set_fits()
+    set_baseline_fits()
+    
+    result = whiff(ncounts=mx.whiff.counts, conditionals=mx.whiff.conditionals)
+    wab=1.0
+    if result=='run':
+       post_equilibration()
+       wab = mx.whiff.abbreviated_count_ratio
+    elif result=='run_remainder':
+       reset_measurement(ACTIVE_DETECTORS)
+       open(mx.equilibration.outlet)
+       sleep(15)
+       meqtime = eqtime if mx.equlibration.use_extraction_eqtime else mx.equilibration.eqtime
+       equilibrate(eqtime=meqtime, do_post_equilibration=True,
+                inlet=mx.equilibration.inlet, outlet=mx.equilibration.outlet)
+       set_time_zero(0)
+       sniff(meqtime)
+       
+    multicollect(ncounts=mx.multicollect.counts*wab, integration_time=1)
+    if mx.baseline.after:
+        baselines(ncounts=mx.baseline.counts*wab, mass=mx.baseline.mass, detector=mx.baseline.detector)
+
+    if mx.peakcenter.after:
+        peak_center(detector=mx.peakcenter.detector,isotope=mx.peakcenter.isotope)
+    info('finished measure script')
+
+#========================EOF==============================================================
+    #peak_hop(detector='CDD', isotopes=['Ar40','Ar39','Ar36'], cycles=2, integrations=3)
+    #baselines(counts=50,mass=0.5, detector='CDD')s
+
+#isolate sniffer volume
+    # close('S')
+#     sleep(1)
+#
+#     #open to mass spec
+#     open('R')
+#
+#     set_time_zero()
+#     #display pressure wave
+#     sniff(5)
+#
+#     #define sniff/split threshold
+#     sniff_threshold=100
+#
+#     #test condition
+#     #if get_intensity('H1')>sniff_threshold:
+#     if True:
+#         gosub('splits:jan_split', klass='ExtractionLinePyScript')
+#
